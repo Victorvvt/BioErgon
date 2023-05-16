f9242fb47f713036afad25f828b85d98275655ee
Victor <vvtdeveloper@gmail.com>
2023-05-16 12:54:02 +0200

    correccion delete

--- js/search-crud-usuarios.js
+++ js/search-crud-usuarios.js
@@ -1,41 +1,41 @@
 
 $(function () {
-    $('#form-usuarios').submit(function (e) {
-        e.preventDefault();
-        location.reload();
-    
-        let postData = {
-          nombre: $('#nombre').val(),
-          apellido: $('#apellido').val(),
-          peso: $('#peso').val(),
-          altura: $('#altura').val(),
-          sexo: $('#sexo').val(),
-          actividad: $('#actividad').val(),
-          fecha: $('#fecha').val()
-    
+  $('#form-usuarios').submit(function (e) {
+    e.preventDefault();
+    location.reload();
+
+    let postData = {
+      nombre: $('#nombre').val(),
+      apellido: $('#apellido').val(),
+      peso: $('#peso').val(),
+      altura: $('#altura').val(),
+      sexo: $('#sexo').val(),
+      actividad: $('#actividad').val(),
+      fecha: $('#fecha').val()
+
+    }
+    $.ajax({
+      url: "http://localhost/Biergon1.0/php/insert-usuarios.php",
+      data: postData,
+      type: "POST",
+      success: function (response) {
+        if (!response.error) {
+          $('#form-usuarios').trigger("reset");
         }
-        $.ajax({
-          url: "http://localhost/Biergon1.0/php/insert-usuarios.php",
-          data: postData,
-          type: "POST",
-          success: function (response) {
-            if (!response.error) {
-              $('#form-usuarios').trigger("reset");
-            }
-          }
-        });
-    
-      });
-    function get(){
-        $.ajax({
-            type: "get",
-            url: "http://localhost/Biergon1.0/php/select-usuarios.php",
-        
-            success: function (response) {
-              let usuarios = JSON.parse(response);
-              let template = ``;
-              usuarios.forEach(usuario => {
-                template+=`
+      }
+    });
+
+  });
+  function get() {
+    $.ajax({
+      type: "get",
+      url: "http://localhost/Biergon1.0/php/select-usuarios.php",
+
+      success: function (response) {
+        let usuarios = JSON.parse(response);
+        let template = ``;
+        usuarios.forEach(usuario => {
+          template += `
                   <tr id="${usuario.id}">
                   <td>${usuario.id}</td>
                   <td>${usuario.nombre}</td>
@@ -47,28 +47,28 @@ $(function () {
                   <td>${usuario.fecha}</td>
                   <td><button class="btn btn-danger delete-usuario" >Eliminar</button></td>
                   </tr>
-                `    
-                });
-                $('#res').html(template);
-            }
-          });
-    }
-    get();
-    // search 
-    $('#search').keyup(() => {
-        if ($("#search").val()) {
-            let search = $("#search").val();
-            
-            $.ajax({
-                url: "http://localhost/Biergon1.0/php/search-usuario.php",
-                data: { search },
-                type: "POST",
-                success: function (response) {
-                    if (!response.error) {
-                        let usuarios = JSON.parse(response);
-                        let template = ``;
-                        usuarios.forEach(usuario => {
-                            template += `<tr>
+                `
+        });
+        $('#res').html(template);
+      }
+    });
+  }
+  get();
+  // search 
+  $('#search').keyup(() => {
+    if ($("#search").val()) {
+      let search = $("#search").val();
+
+      $.ajax({
+        url: "http://localhost/Biergon1.0/php/search-usuario.php",
+        data: { search },
+        type: "POST",
+        success: function (response) {
+          if (!response.error) {
+            let usuarios = JSON.parse(response);
+            let template = ``;
+            usuarios.forEach(usuario => {
+              template += `<tr>
                     <td>${usuario.id}</td>
                     <td>${usuario.nombre}</td>
                     <td>${usuario.apellido}</td>
@@ -78,38 +78,39 @@ $(function () {
                     <td>${usuario.peso}</td>
                     <td>${usuario.fecha}</td>
                     </tr>`
-                        });
-                        $('#res').html(template);
-
-
-                    }
-                }
-
             });
-        }else{
-            get()
-    
-        } 
+            $('#res').html(template);
 
-    });
 
-    //delete
- $(document).click(()=>{
+          }
+        }
 
-   if(confirm("¿Seguro quieres eliminiar registro?")){
-     const element = $(this)[0].activeElement.parentElement.parentElement;
-    const id = $(element).attr('id')
-    $.post("url", data,
-      function (data, textStatus, jqXHR) {
-        
-      },
-      "dataType"
-    );
+      });
+    } else {
+      get()
+
     }
-    
-   })
 
- 
+  });
+
+  //delete
+  $(document).on("click", ".delete-usuario", () => {
+
+    if (confirm("¿Seguro quieres eliminiar registro?")) {
+      const element = $(this)[0].activeElement.parentElement.parentElement;
+      const id = $(element).attr('id')
+      $.post("http://localhost/Biergon1.0/php/delete-usuarios.php",
+      {id},
+       ()=>{
+          get();
+        }
+        
+      );
+    }
+
+  })
+
+
 
 
 })
